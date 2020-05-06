75aa2920b6fc7afc9a04ad1f2d8115e908ba4297
Aida Nadine Gaita <gaitanadine@yahoo.com>
2020-05-06 19:50:23 +0300

    splitting homepage into smaller components, finished styling our menu-item component

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,63 @@
+import React, {Component} from 'react';
+import MenuItem from '../menu-item/menu-item.component'
+import './directory.styles.scss'
+import fatloss from './../../assets/fatloss.png'
+import musclebuilding from './../../assets/musclebuilding.png'
+import cardio from './../../assets/cardio.png'
+import yoga from './../../assets/yoga.png'
+import pilates from './../../assets/pilates.png'
+
+class Directory extends Component {
+    constructor(){
+        super()
+
+        this.state= {
+            sections:[
+                {
+                  title: 'fat loss',
+                  imageUrl: `${fatloss}`,
+                  id: 1,
+                  linkUrl: 'shop/hats'
+                },
+                {
+                  title: 'muscle building',
+                  imageUrl: `${musclebuilding}`,
+                  id: 2,
+                  linkUrl: 'shop/jackets'
+                },
+                {
+                  title: 'cardio',
+                  imageUrl: `${cardio}`,
+                  id: 3,
+                  linkUrl: 'shop/sneakers'
+                },
+                {
+                  title: 'yoga',
+                  imageUrl: `${yoga}`,
+                  size: 'large',
+                  id: 4,
+                  linkUrl: 'shop/womens'
+                },
+                {
+                  title: 'pilates',
+                  imageUrl: `${pilates}`,
+                  size: 'large',
+                  id: 5,
+                  linkUrl: 'shop/mens'
+                }
+            ]
+        }
+    }
+
+    render(){
+        return(
+            <div className='directory-menu'>
+                {this.state.sections.map(section => (
+                    <MenuItem key={section.id} title={section.title} imageUrl={section.imageUrl} size={section.size}/>
+                ))}
+            </div>
+        )
+    }
+}
+
+export default Directory;
