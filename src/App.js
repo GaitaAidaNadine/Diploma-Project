19d83099f1ec2d368d1a8a11de399a09070f7993
Aida Nadine Gaita <gaitanadine@yahoo.com>
2020-05-07 12:46:29 +0300

    update menu-item component to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homepage/homepage.component";
+import "./App.css";
+
+const FatLoss = () => (
+  <div>
+    <h1>FAT LOSS</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/fatloss' component={FatLoss} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,63 +1,69 @@
-import React, {Component} from 'react';
-import MenuItem from '../menu-item/menu-item.component'
-import './directory.styles.scss'
-import fatloss from './../../assets/fatloss.png'
-import musclebuilding from './../../assets/musclebuilding.png'
-import cardio from './../../assets/cardio.png'
-import yoga from './../../assets/yoga.png'
-import pilates from './../../assets/pilates.png'
+import React, { Component } from "react";
+import MenuItem from "../menu-item/menu-item.component";
+import "./directory.styles.scss";
+import fatloss from "./../../assets/fatloss.png";
+import musclebuilding from "./../../assets/musclebuilding.png";
+import cardio from "./../../assets/cardio.png";
+import yoga from "./../../assets/yoga.png";
+import pilates from "./../../assets/pilates.png";
 
 class Directory extends Component {
-    constructor(){
-        super()
+  constructor() {
+    super();
 
-        this.state= {
-            sections:[
-                {
-                  title: 'fat loss',
-                  imageUrl: `${fatloss}`,
-                  id: 1,
-                  linkUrl: 'shop/hats'
-                },
-                {
-                  title: 'muscle building',
-                  imageUrl: `${musclebuilding}`,
-                  id: 2,
-                  linkUrl: 'shop/jackets'
-                },
-                {
-                  title: 'cardio',
-                  imageUrl: `${cardio}`,
-                  id: 3,
-                  linkUrl: 'shop/sneakers'
-                },
-                {
-                  title: 'yoga',
-                  imageUrl: `${yoga}`,
-                  size: 'large',
-                  id: 4,
-                  linkUrl: 'shop/womens'
-                },
-                {
-                  title: 'pilates',
-                  imageUrl: `${pilates}`,
-                  size: 'large',
-                  id: 5,
-                  linkUrl: 'shop/mens'
-                }
-            ]
-        }
-    }
+    this.state = {
+      sections: [
+        {
+          title: "fat loss",
+          imageUrl: `${fatloss}`,
+          id: 1,
+          linkUrl: "fatloss",
+        },
+        {
+          title: "muscle building",
+          imageUrl: `${musclebuilding}`,
+          id: 2,
+          linkUrl: "",
+        },
+        {
+          title: "cardio",
+          imageUrl: `${cardio}`,
+          id: 3,
+          linkUrl: "",
+        },
+        {
+          title: "yoga",
+          imageUrl: `${yoga}`,
+          size: "large",
+          id: 4,
+          linkUrl: "",
+        },
+        {
+          title: "pilates",
+          imageUrl: `${pilates}`,
+          size: "large",
+          id: 5,
+          linkUrl: "",
+        },
+      ],
+    };
+  }
 
-    render(){
-        return(
-            <div className='directory-menu'>
-                {this.state.sections.map(section => (
-                    <MenuItem key={section.id} title={section.title} imageUrl={section.imageUrl} size={section.size}/>
-                ))}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.sections.map((section) => (
+          <MenuItem
+            key={section.id}
+            title={section.title}
+            imageUrl={section.imageUrl}
+            size={section.size}
+            linkUrl={section.linkUrl}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Directory;
